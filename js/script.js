791a05a262858553473d48b95a2c9ef01cadbee4
k-gmcmahan <48691326+k-gmcmahan@users.noreply.github.com>
2019-04-12 15:07:02 -0400

    add hamburger menu function
    add hamburger menu function for mobile view

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
 $(document).ready(function() {
   $(".project-preview").on("click", function() {
     $(this).siblings(".project-details").slideToggle();
