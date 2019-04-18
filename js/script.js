24e22511147f5bc2379d97f34c91fe61abdd65ec
k-gmcmahan <48691326+k-gmcmahan@users.noreply.github.com>
2019-04-18 12:03:34 -0400

    Add a "Back to Top" button on all pages

--- js/script.js
+++ js/script.js
@@ -12,3 +12,19 @@ $(document).ready(function() {
     $(this).siblings(".project-details").slideToggle();
   });
 });
+
+var $backToTop = $(".back-to-top");
+$backToTop.hide();
+
+
+$(window).on('scroll', function() {
+  if ($(this).scrollTop() > 100) {
+    $backToTop.fadeIn();
+  } else {
+    $backToTop.fadeOut();
+  }
+});
+
+$backToTop.on('click', function(e) {
+  $("html, body").animate({scrollTop: 0}, 500);
+});
